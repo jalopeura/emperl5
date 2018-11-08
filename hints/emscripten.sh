32197c61c86fb887738ab16f4e1bdaad58093fdc
Hauke D <haukex@zero-g.net>
2018-11-08 14:13:09 +0100

    Adding Digest::MD5 and Digest::SHA modules
    Once this is released, it will close haukex/webperl#5.
    Thanks, @AlDanial!

--- hints/emscripten.sh
+++ hints/emscripten.sh
@@ -73,7 +73,7 @@ dlsrc='none'
 # Sys/Hostname Sys/Syslog threads threads/shared Tie/Hash/NamedCapture
 # Time/HiRes Time/Piece Unicode/Collate Unicode/Normalize XS/APItest XS/Typemap]  
 #TODO Later: Reinsert Storable after Socket, its Makefile seems to not work in our environment
-static_ext="attributes B Cwd Data/Dumper Devel/Peek Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/scalar PerlIO/via POSIX re SDBM_File Socket Tie/Hash/NamedCapture Time/HiRes Time/Piece Unicode/Normalize WebPerl $EMPERL_STATIC_EXT"
+static_ext="attributes B Cwd Data/Dumper Devel/Peek Digest/MD5 Digest/SHA Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/scalar PerlIO/via POSIX re SDBM_File Socket Tie/Hash/NamedCapture Time/HiRes Time/Piece Unicode/Normalize WebPerl $EMPERL_STATIC_EXT"
 dynamic_ext=''
 
 # It *looks* like shm*, sem* and a few others exist in Emscripten's libc,
